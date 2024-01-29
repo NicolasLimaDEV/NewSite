fb7dc5df85db52935c407fd2607b35c4596d7071
Nicolas Lima <91435296+NicolasLimaDEV@users.noreply.github.com>
2024-01-29 12:04:28 -0300

    Atualizando BR texto sobre

--- src/components/Sobre/index.jsx
+++ src/components/Sobre/index.jsx
@@ -7,7 +7,7 @@ const SobreMim = () => {
       <p>
         Comecei a explorar o mundo da tecnologia em 2021, iniciando com cursos voltados para Front-end (HTML, CSS, Javascript), logo em seguida fui me aprofundar até conseguir meu primeiro emprego em 2022. 
 
-        Sempre estudando as ferramentas atuais do mercado e em busca de evolução.
+        Sempre estudando as ferramentas atuais do mercado e em busca de evolução. </br>
         Em 2023 dei inicio ao curso de Análise e Desenvolvimento de Sistemas pela Faculdade Impacta. E junto iniciei a minha segunda experiência como Desenvolvedor Web. 
 
         Possuo facilidade de aprender coisas novas e me adaptar a ambientes novos.
